1c4b1a218256cfeebb6803c73b9ffaf578411da7
nakaji <nakajinia0501@gmail.com>
2020-12-09 13:06:55 +0900

    add  + routing

---
+++ qa/resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QA掲示板</title>
+</head>
+<body>
+    <h1>Hello QA SITE</h1>
+</body>
+</html>
