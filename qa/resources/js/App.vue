f220f10c0e96c4bdbbff21156300ee1637126a3f
nakaji <nakajinia0501@gmail.com>
2020-12-09 14:10:43 +0900

    routing in vueRouter + add  mainPages

---
+++ qa/resources/js/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+
+<header>
+
+</header>
+
+<main>
+
+<div class="container">
+<router-view />
+</div>
+
+</main>
+
+<footer>
+
+</footer>
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
