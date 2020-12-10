734590d3628e42cf1035a880de9dfbc839f37cab
nakaji <nakajinia0501@gmail.com>
2020-12-10 13:08:48 +0900

    Sort.vueを追加

---
+++ qa/resources/js/components/Sort.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="sort_container">
+        <p>人気順</p>
+        <p>新着順</p>
+    </div>
+</template>
+
+<style>
+.sort_container {
+    display: flex;
+}
+</style>
--- qa/resources/js/pages/BoardList.vue
+++ qa/resources/js/pages/BoardList.vue
@@ -2,15 +2,18 @@
 <div>
     <h1>boardlist</h1>
     <Tag />
+    <Sort />
 </div>
 </template>
 
 <script>
 import Tag from '../components/Tag'
+import Sort from '../components/sort'
 
 export default {
     components: {
         Tag,
+        Sort,
     }
 }
 </script>
