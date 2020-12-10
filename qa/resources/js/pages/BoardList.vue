f1415d24376a9f67c60fc4e24771a03da4791207
nakaji <nakajinia0501@gmail.com>
2020-12-10 13:44:00 +0900

    Board.vueを追加

--- qa/resources/js/pages/BoardList.vue
+++ qa/resources/js/pages/BoardList.vue
@@ -1,19 +1,22 @@
 <template>
-<div>
+<div class="container">
     <h1>boardlist</h1>
     <Tag />
     <Sort />
+    <Board />
 </div>
 </template>
 
 <script>
 import Tag from '../components/Tag'
 import Sort from '../components/sort'
+import Board from  '../components/Board'
 
 export default {
     components: {
         Tag,
         Sort,
+        Board,
     }
 }
 </script>
