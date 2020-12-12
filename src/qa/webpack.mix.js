364868ac72b4f01f5f2a869ef1cea3c82fde2fff
nakaji <nakajinia0501@gmail.com>
2020-12-12 22:12:25 +0900

    ブラウザ表示されないエラー修正中

--- src/qa/webpack.mix.js
+++ src/qa/webpack.mix.js
@@ -11,9 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.browserSync({
-    proxy: '0.0.0.0:8082',
-    open: false
-    })
-    js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
