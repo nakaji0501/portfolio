1082016bb4480fbef90943fb817509327fba49e8
nakaji <nakajinia0501@gmail.com>
2020-12-12 12:03:11 +0900

    loginApiTest OK

--- src/qa/routes/api.php
+++ src/qa/routes/api.php
@@ -19,3 +19,7 @@ use Illuminate\Http\Request;
 
 // 会員登録
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
+
