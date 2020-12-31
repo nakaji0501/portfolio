7fba4068c0ae93affa45c7c52c6a93f7f404c1cd
nakaji <nakajinia0501@gmail.com>
2021-01-01 00:04:04 +0900

    add: ページネーション機能コンポーネントを追加

---
+++ src/qa/resources/js/components/Pagination.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="pagination">
+
+        <router-link class="button"
+        v-if="! isFirstPage"
+        :to="`/?page=${currentPage - 1}`"
+        >
+        &laquo; prev
+        </router-link>
+
+        <router-link class="button"
+        v-if="! isLastPage"
+        :to="`/?page=${currentPage + 1}`"
+        >
+        next &raquo;
+        </router-link>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        currentPage: {
+            type: Number,
+            required: true
+        },
+        lastPage: {
+            type: Number,
+            required: true
+        }
+    },
+    computed: {
+        isFirstPage() {
+            return this.currentPage === 1
+        },
+        isLastPage() {
+            return this.currentPage === this.lastPage
+        }
+    }
+}
+</script>
