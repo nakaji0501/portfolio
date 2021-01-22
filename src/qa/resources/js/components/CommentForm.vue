fa92d27c6d3032d1663945ebd175b512ead1d8bb
nakaji <nakajinia0501@gmail.com>
2021-01-22 13:15:08 +0900

    style: 返信フォームのスタイルを修正 + テキストエリアの行高自動調整メソッドを追加

--- src/qa/resources/js/components/CommentForm.vue
+++ src/qa/resources/js/components/CommentForm.vue
@@ -1,22 +1,24 @@
 <template>
-    <div class="commentForm_container">
+    <div class="commentForm">
 
         <div class="loader">
             <Loader
             v-show="posting"
             >
-                <template slot="loadingText">
+                <template class="loader_text"
+                slot="loadingText"
+                >
                     コメントを投稿中です。しばらくお待ちください。
                 </template>
             </Loader>
         </div>
 
-        <form class="commentForm"
+        <form class="commentForm_from"
         @submit.prevent="addComment"
         v-if="isLogin"
         v-show="! posting"
         >
-            <div
+            <div class="error"
             v-if="commentErrors"
             >
                 <ul v-if="commentErrors.message">
@@ -28,24 +30,26 @@
                 </ul>
             </div>
 
-            <div
+            <div class="commentForm_text"
             v-else
             >
                 <p>返信を書いてください</p>
             </div>
 
-            <div>
-                <label for="comment"></label>
-                <textarea name="comment" id="comment" cols="30" rows="10"
+            <div class="commentForm_message textarea">
+                <textarea class="textarea_body" name="comment" id="comment"
                 v-model="commentMessage"
+                ref="area"
+                :style="styles"
                 >
                 </textarea>
-                <div class="button">
-                    <button type="submit">送信</button>
-                </div>
             </div>
-        </form>
-    </div>
+
+            <div class="commentForm_button">
+                <button class="button" type="submit">送信</button>
+            </div>
+        </form><!-- /commentForm_from -->
+    </div><!-- /commentForm -->
 </template>
 
 <script>
@@ -68,6 +72,7 @@ export default {
     data() {
         return {
             commentMessage: '',
+            height: "240px",
             commentErrors: '',
             posting: false,
         }
@@ -104,17 +109,78 @@ export default {
                 response.data,
                 ...this.question.comments
             ]
-        }
+        },
+        resizeCommentMessage() {
+            this.height = this.$refs.area.scrollHeight + 'px';
+            this.height = 'auto';
+            this.$nextTick(() => {
+                this.height = this.$refs.area.scrollHeight + 'px';
+            });
+        },
     },
     computed: {
         isLogin() {
             return this.$store.getters['auth/check']
+        },
+        styles() {
+            return {
+                "height": this.height,
+            }
+        },
+    },
+    watch: {
+        commentMessage() {
+            this.resizeCommentMessage();
         }
-    }
+    },
+    mounted() {
+        this.resizeCommentMessage();
+    },
 }
 </script>
 
 <style lang="scss" scoped>
+.textarea {
+    line-height: 1.8;
+    &_body {
+        width: 100%;
+        border: 1px solid #b6c3c6;
+        border-radius: 16px;
+        font: inherit;
+        &:focus {
+            box-shadow: 0 0 0 4px rgba(35, 167, 195, 0.3);
+            outline: 0;
+        }
+    }
+}
+.commentForm {
+    &_button {
+        margin-top: 24px;
+        text-align: center;
+    }
+}
+.button {
+    position: relative;
+    display: inline-block;
+    padding: 0.25em 0.5em;
+    font-size: 1.2rem;
+    text-decoration: none;
+    color: #FFF;
+    background: #fd9535;/*色*/
+    border-radius: 4px;/*角の丸み*/
+    box-shadow: inset 0 2px 0 rgba(255,255,255,0.2), inset 0 -2px 0 rgba(0, 0, 0, 0.05);
+    font-weight: bold;
+    border: solid 2px #d27d00;/*線色*/
+    cursor: pointer;
+    &:active {
+        box-shadow: 0 0 2px rgba(0, 0, 0, 0.30);
+    }
+}
+.error * {
+    font-size: 1.2rem;
+    font-weight: bold;
+    color: red;
+}
 @media screen and (max-width: 480px) {
 
 }
