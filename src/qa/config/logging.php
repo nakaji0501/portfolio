7cc6b8f15b9dedeac79332a3e42e311e68bebfe2
nakaji <nakajinia0501@gmail.com>
2021-02-04 15:45:04 +0900

    update: composer version up

--- src/qa/config/logging.php
+++ src/qa/config/logging.php
@@ -18,16 +18,6 @@ return [
     */
 
     'default' => env('LOG_CHANNEL', 'stack'),
-    'channels' => [
-        'stack' => [
-            'driver' => 'stack',
-            'channels' => ['single'],
-        ],
-        'single' => [
-            'driver' => 'errorlog',
-            'level' => 'debug'
-        ],
-    ]
 
     /*
     |--------------------------------------------------------------------------
