657b4d66210267ffe39e3b5943a8ac4ce39adb0e
nakaji <nakajinia0501@gmail.com>
2021-02-06 17:34:07 +0900

    fix: Mixed content error 再度解消

--- src/qa/public/js/app.js
+++ src/qa/public/js/app.js
@@ -17796,7 +17796,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
             switch (_context.prev = _context.next) {
               case 0:
                 _context.next = 2;
-                return axios.get("/api/questions/?page=".concat(_this.page));
+                return axios.get("/api/questions?page=".concat(_this.page));
 
               case 2:
                 response = _context.sent;
