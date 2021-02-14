58376233cb80399cf748c2af69b2c8498d8c49db
nakaji <nakajinia0501@gmail.com>
2021-02-15 00:45:59 +0900

    add: 写真詳細ページを実装

--- src/qa/resources/js/pages/PhotoDetail.vue
+++ src/qa/resources/js/pages/PhotoDetail.vue
@@ -1,3 +1,51 @@
 <template>
-    <h1>Photo Detail</h1>
+  <div v-if="photo" class="photo-detail">
+    <figure class="photo-detail__pane photo-detail__image">
+      <img :src="photo.url" alt="">
+      <figcaption>Posted by {{ photo.owner.name }}</figcaption>
+    </figure>
+    <div class="photo-detail__pane">
+      <button class="button button--like" title="Like photo">
+        <i class="icon ion-md-heart"></i>12
+      </button>
+    </div>
+  </div>
 </template>
+
+<script>
+import { OK } from '../util'
+
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      photo: null
+    }
+  },
+  methods: {
+    async fetchPhoto () {
+      const response = await axios.get(`/api/photos/${this.id}`)
+
+      if (response.status !== OK) {
+        this.$store.commit('error/setCode', response.status)
+        return false
+      }
+
+      this.photo = response.data
+    }
+  },
+  watch: {
+    $route: {
+      async handler () {
+        await this.fetchPhoto()
+      },
+      immediate: true
+    }
+  }
+}
+</script>
